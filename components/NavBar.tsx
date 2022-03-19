5d76453a5f9c58e8ded24a998a6af154f766fcb9
Varun Chodanker <57418924+varunchodanker@users.noreply.github.com>
2022-03-19 00:30:25 +0000

    Better nav spacing for non-mobile sizes.

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -7,17 +7,17 @@ const NavBar = () => {
     return (
             <AppBar position='sticky' className = "navAppBar">
                     <Toolbar disableGutters className = "navToolBar">
-                        <Typography variant = 'h5' sx = {{ml: '3vw', flexGrow: 1}}
+                        <Typography className = "navTitle" variant = 'h5' sx = {{mr: "auto"}}
                         >
                             Productivity Organiser
                         </Typography>
                         <Button className = "accountButton" variant = "outlined" color = "inherit" sx = {{mr: '1.2vw', size: "small"}}>
-                            <Typography variant = 'h6'>
+                            <Typography variant = 'h7'>
                                 Login
                             </Typography>
                         </Button>
                         <Button className = "accountButton" variant = "outlined" color = "inherit" sx = {{mr: '1.2vw', size: "small"}}>
-                            <Typography variant = 'h6'>
+                            <Typography variant = 'h7'>
                                 Signup
                             </Typography>
                         </Button>
