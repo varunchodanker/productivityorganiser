8d0bbb420898d75e0dd8b58cc388bb728bc121a0
Varun Chodanker <57418924+varunchodanker@users.noreply.github.com>
2022-03-19 01:34:55 +0000

    Centering and icon.

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,16 +2,18 @@ import { Button } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import ScheduleIcon from '@mui/icons-material/Schedule';
 
 const NavBar = () => {
     return (
             <AppBar position='sticky' className = "navAppBar">
                     <Toolbar disableGutters className = "navToolBar">
+                        <ScheduleIcon id = "navIcon" sx = {{position: "relative", left: "1.2vw", fontSize: "2.5em"}} />
                         <Typography className = "navTitle" variant = 'h5' sx = {{mr: "auto"}}
                         >
                             Productivity Organiser
                         </Typography>
-                        <Button className = "accountButton" variant = "outlined" color = "inherit" sx = {{mr: '1.2vw', size: "small"}}>
+                        <Button className = "accountButton" variant = "outlined" color = "inherit" sx = {{ml: "auto", mr: '1.2vw', size: "small"}}>
                             <Typography variant = 'h7'>
                                 Login
                             </Typography>
