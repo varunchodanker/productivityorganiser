2ac532c0946659c8219353ae2e51b663893ff288
Varun Chodanker <57418924+varunchodanker@users.noreply.github.com>
2022-03-19 01:40:07 +0000

    Centered title wrt extreme areas.

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -9,11 +9,11 @@ const NavBar = () => {
             <AppBar position='sticky' className = "navAppBar">
                     <Toolbar disableGutters className = "navToolBar">
                         <ScheduleIcon id = "navIcon" sx = {{position: "relative", left: "1.2vw", fontSize: "2.5em"}} />
-                        <Typography className = "navTitle" variant = 'h5' sx = {{mr: "auto"}}
+                        <Typography className = "navTitle" variant = 'h5' sx = {{mr: "auto", ml: "auto"}}
                         >
                             Productivity Organiser
                         </Typography>
-                        <Button className = "accountButton" variant = "outlined" color = "inherit" sx = {{ml: "auto", mr: '1.2vw', size: "small"}}>
+                        <Button className = "accountButton" variant = "outlined" color = "inherit" sx = {{mr: '1.2vw', size: "small"}}>
                             <Typography variant = 'h7'>
                                 Login
                             </Typography>
