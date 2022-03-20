2945e1209eba7c89dd888f2428d2714932e48693
Varun Chodanker <57418924+varunchodanker@users.noreply.github.com>
2022-03-20 19:43:44 +0000

    Initial date picker.

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,15 @@ import styles from '../styles/Home.module.css'
 import NavBar from '../components/NavBar'
 import { Box } from '@mui/material'
 
+import TextField from '@mui/material/TextField';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import StaticDatePicker from '@mui/lab/StaticDatePicker';
+import React from 'react'
+
 const Home: NextPage = () => {
+  const [targetDate, setTargetDate] = React.useState<Date | null>(new Date());
+
   return (
     <>
       <Head>
@@ -16,6 +24,17 @@ const Home: NextPage = () => {
       <main>
         <Box id = "contentArea">
           This is where the content will go.
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <StaticDatePicker<Date>
+              orientation="portrait"
+              openTo="day"
+              value={targetDate}
+              onChange={(newTargetDate) => {
+                setTargetDate(newTargetDate);
+              }}
+              renderInput={(params) => <TextField {...params} />}
+            />
+          </LocalizationProvider>
         </Box>
       </main>
 
