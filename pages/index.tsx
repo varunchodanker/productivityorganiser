01b041cacf929796eedcffcb839ff0c5dd4fca28
Varun Chodanker <57418924+varunchodanker@users.noreply.github.com>
2022-03-20 01:46:42 +0000

    Initial content area.

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import NavBar from '../components/NavBar'
+import { Box } from '@mui/material'
 
 const Home: NextPage = () => {
   return (
@@ -13,7 +14,9 @@ const Home: NextPage = () => {
       </Head>
       <NavBar></NavBar>
       <main>
-        
+        <Box id = "contentArea">
+          This is where the content will go.
+        </Box>
       </main>
 
       <footer>
