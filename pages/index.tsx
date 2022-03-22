fa24cf6bee2874f6061e66c5ac6132286fd14b65
Varun Chodanker <57418924+varunchodanker@users.noreply.github.com>
2022-03-22 00:18:18 +0000

    Right action tabs.

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import NavBar from '../components/NavBar'
-import { Box } from '@mui/material'
+import { Box, Tab, Tabs, Typography } from '@mui/material'
 
 import TextField from '@mui/material/TextField';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
@@ -11,8 +11,47 @@ import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import StaticDatePicker from '@mui/lab/StaticDatePicker';
 import React from 'react'
 
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
 const Home: NextPage = () => {
   const [targetDate, setTargetDate] = React.useState<Date | null>(new Date());
+
+  const [chosenTab, setChosenTab] = React.useState(0);
+
+  const handleTabChange = (event: React.SyntheticEvent, newChosenTab: number) => {
+    setChosenTab(newChosenTab);
+  };
 
   return (
     <>
@@ -37,7 +76,22 @@ const Home: NextPage = () => {
             </LocalizationProvider>
           </Box>
           <Box className = "halfMainArea">
-
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+              <Tabs value={chosenTab} onChange={handleTabChange} aria-label="basic tabs example">
+                <Tab label="Item One" {...a11yProps(0)} />
+                <Tab label="Item Two" {...a11yProps(1)} />
+                <Tab label="Item Three" {...a11yProps(2)} />
+              </Tabs>
+            </Box>
+            <TabPanel value={chosenTab} index={0}>
+              Item One
+            </TabPanel>
+            <TabPanel value={chosenTab} index={1}>
+              Item Two
+            </TabPanel>
+            <TabPanel value={chosenTab} index={2}>
+              Item Three
+            </TabPanel>
           </Box>
         </Box>
       </main>
